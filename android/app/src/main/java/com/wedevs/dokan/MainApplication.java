a9d77183cd4fd733d1e9b8419c74930b66e2bcbf
ibnulK <ikayes114@gmail.com>
2019-01-23 18:04:51 +0600

    Upgrade android build system

--- android/app/src/main/java/com/wedevs/dokan/MainApplication.java
+++ android/app/src/main/java/com/wedevs/dokan/MainApplication.java
@@ -41,7 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new RNSharePackage(),
+        new RNSharePackage(),
         new FastImageViewPackage(),
         new RNFetchBlobPackage(),
         new ReactNativeRestartPackage(),
