af0cae571bce15a9bc5e980504c25c73eec3f5c1
ibnulK <ikayes114@gmail.com>
2019-05-07 09:06:42 +0600

    Combine About, Contact and Privacy Policy Screen

--- src/navigation/AboutUsScreen.js
+++ src/navigation/AboutUsScreen.js
@@ -3,11 +3,20 @@
  */
 
 import React, { PureComponent } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import {
+	View,
+	Text,
+	Image,
+	StyleSheet,
+	Dimensions,
+	TouchableWithoutFeedback,
+	Linking,
+} from 'react-native';
+import { Avatar } from 'react-native-paper';
 import { Back, HeaderRight } from './IconNav';
-import { LinearGradient } from '@expo';
-import { Images, Color, Styles, Config } from '@common';
-import { CustomPage } from '@containers';
+import { Images, Color, Styles, Config, Constant } from '@common';
+import { ImageCache } from '@components';
+import { version } from '../../package.json';
 
 export default class AboutUsScreen extends PureComponent {
 	static navigationOptions = ({ navigation }) => ({
@@ -23,106 +32,132 @@ export default class AboutUsScreen extends PureComponent {
 	});
 
 	render() {
-		// const { navigate } = this.props.navigation;
-		// const rootNavigation = this.props.screenProps.rootNavigation;
-
 		return (
-			<View style={{ flex: 1, alignItems: 'center' }}>
-				{/* <View style={styles.slimTabContainer}>
-					<View style={styles.slimTabCotainer}>
-						<LinearGradient
-							style={styles.slimTabCircle}
-							colors={['#F9769D', '#BB6DF7']}>
-							<Image
-								source={Images.IcVision}
-								style={{ width: 25, height: 25 }}
-								resizeMode="contain"
-							/>
-						</LinearGradient>
-						<View style={styles.vendorInfo}>
-							<Text style={{ fontSize: 18, marginLeft: 5, color: '#000' }}>
-								Vision
-							</Text>
-							<Text style={{ color: '#818995', marginLeft: 5, marginTop: 10 }}>
-								Short Description
-							</Text>
-						</View>
+			<View
+				style={{
+					flex: 1,
+					alignItems: 'center',
+					// justifyContent: 'center',
+					backgroundColor: '#fff',
+				}}>
+				<View style={styles.logoWrap}>
+					{Config.appSettings.app_logo ? (
+						<ImageCache
+							uri={Config.appSettings.app_logo}
+							style={styles.logo}
+							resizeMode="contain"
+						/>
+					) : (
+						<Image
+							source={Config.LogoWithText}
+							style={styles.logo}
+							resizeMode="contain"
+						/>
+					)}
+					<Text style={styles.logoText}>Running Dokan v{version}</Text>
+				</View>
+				<View style={styles.linkContainer}>
+					<View style={styles.links}>
+						<TouchableWithoutFeedback
+							hitSlop={{ top: 0, left: 0, bottom: 0, right: 0 }}
+							onPress={() => Linking.openURL('https://wedevs.com/contact/')}>
+							<Text style={styles.linkText}>Contact</Text>
+						</TouchableWithoutFeedback>
 					</View>
-
-					<View style={styles.slimTabCotainer}>
-						<LinearGradient
-							style={styles.slimTabCircle}
-							colors={['#32D1DC', '#6F9BF5']}>
-							<Image
-								source={Images.IcLicense}
-								style={{ width: 25, height: 25 }}
-								resizeMode="contain"
-							/>
-						</LinearGradient>
-						<View style={styles.vendorInfo}>
-							<Text style={{ fontSize: 18, marginLeft: 5, color: '#000' }}>
-								License
-							</Text>
-							<Text style={{ color: '#818995', marginLeft: 5, marginTop: 10 }}>
-								Short Description
-							</Text>
-						</View>
+					<View style={styles.links}>
+						<TouchableWithoutFeedback
+							hitSlop={{ top: 0, left: 0, bottom: 0, right: 0 }}
+							onPress={() =>
+								Linking.openURL('https://wedevs.com/privacy-policy/')
+							}>
+							<Text style={styles.linkText}>Privacy Policy</Text>
+						</TouchableWithoutFeedback>
 					</View>
-
-					<View style={styles.slimTabCotainer}>
-						<LinearGradient
-							style={styles.slimTabCircle}
-							colors={['#FAD961', '#F76B1C']}>
-							<Image
-								source={Images.IcPrivacy}
-								style={{ width: 25, height: 25 }}
-								resizeMode="contain"
-							/>
-						</LinearGradient>
-						<View style={styles.vendorInfo}>
-							<Text style={{ fontSize: 18, marginLeft: 5, color: '#000' }}>
-								Privacy Policy
-							</Text>
-							<Text style={{ color: '#818995', marginLeft: 5, marginTop: 10 }}>
-								Short Description
-							</Text>
-						</View>
-					</View>
-        </View> */}
-				<CustomPage id={Config.appSettings.about_us_page_id} />
+					{/* <View style={styles.socialIconContainer}>
+						<Avatar.Icon
+							style={styles.socialIcon}
+							size={50}
+							color="#fff"
+							icon={({ size, color }) => (
+								<Image
+									source={Images.FBlogo}
+									style={{ width: size, height: size, tintColor: color }}
+								/>
+							)}
+						/>
+						<Avatar.Image
+							style={styles.socialIcon}
+							size={50}
+							source={{
+								uri:
+									'https://avatars0.githubusercontent.com/u/17571969?v=3&s=400',
+							}}
+						/>
+						<Avatar.Image
+							style={styles.socialIcon}
+							size={50}
+							source={{
+								uri:
+									'https://avatars0.githubusercontent.com/u/17571969?v=3&s=400',
+							}}
+						/>
+						<Avatar.Image
+							style={styles.socialIcon}
+							size={50}
+							source={{
+								uri:
+									'https://avatars0.githubusercontent.com/u/17571969?v=3&s=400',
+							}}
+						/>
+					</View> */}
+				</View>
 			</View>
 		);
 	}
 }
 
-const styles = StyleSheet.create({
-	slimTabContainer: {
-		width: '90%',
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginTop: 15,
-	},
+const { width, height } = Dimensions.get('window');
 
-	slimTabCotainer: {
-		width: '100%',
-		height: 95,
-		borderRadius: 3,
-		marginBottom: 10,
+const styles = StyleSheet.create({
+	logoWrap: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexGrow: 0.2,
+		// paddingHorizontal: Styles.width * 0.1,
+		// paddingBottom: 50,
+	},
+	logo: {
+		width: width * 0.5,
+		height: (width * 0.5) / 2,
+	},
+	logoText: {
+		// width: width / 2,
+		color: Color.wdgray5,
+	},
+	linkContainer: {
+		marginTop: 50,
+		justifyContent: 'center',
+	},
+	links: {
+		width: width * 0.8,
+		// marginBottom: 15,
+		padding: 15,
+		borderBottomWidth: 1,
+		borderColor: Color.wdgray5,
+		alignItems: 'center',
+	},
+	linkText: {
+		color: Color.blackTextPrimary,
+		fontSize: 18,
+	},
+	socialIconContainer: {
+		width: width * 0.8,
+		marginTop: 50,
 		flexDirection: 'row',
 		alignItems: 'center',
-		justifyContent: 'flex-start',
-		backgroundColor: '#fff',
-		// shadowColor: '#000',
-		// shadowOpacity: 0.2,
-		// shadowOffset: {width: 0, height: 1}
-	},
-	slimTabCircle: {
-		width: 60,
-		height: 60,
-		borderRadius: 30,
-		marginLeft: 15,
-		marginRight: 15,
-		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	socialIcon: {
+		marginHorizontal: 5,
 	},
 });
---
+++ src/navigation/AboutUsScreen_Old.js
@@ -0,0 +1,128 @@
+/**
+ * @format
+ */
+
+import React, { PureComponent } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Back, HeaderRight } from './IconNav';
+import { LinearGradient } from '@expo';
+import { Images, Color, Styles, Config } from '@common';
+import { CustomPage } from '@containers';
+
+export default class AboutUsScreen extends PureComponent {
+	static navigationOptions = ({ navigation }) => ({
+		title: 'About Us',
+		headerLeft: Back(navigation, Images.icons.arrowBack),
+		headerRight: HeaderRight(navigation),
+
+		headerTintColor: Color.headerTintColor,
+		headerStyle: Styles.Common.toolbar,
+		headerLeftContainerStyle: Styles.Common.toolbarLeft,
+		headerRightContainerStyle: Styles.Common.toolbarRight,
+		headerTitleStyle: Styles.Common.headerTitleStyle,
+	});
+
+	render() {
+		// const { navigate } = this.props.navigation;
+		// const rootNavigation = this.props.screenProps.rootNavigation;
+
+		return (
+			<View style={{ flex: 1, alignItems: 'center' }}>
+				{/* <View style={styles.slimTabContainer}>
+					<View style={styles.slimTabCotainer}>
+						<LinearGradient
+							style={styles.slimTabCircle}
+							colors={['#F9769D', '#BB6DF7']}>
+							<Image
+								source={Images.IcVision}
+								style={{ width: 25, height: 25 }}
+								resizeMode="contain"
+							/>
+						</LinearGradient>
+						<View style={styles.vendorInfo}>
+							<Text style={{ fontSize: 18, marginLeft: 5, color: '#000' }}>
+								Vision
+							</Text>
+							<Text style={{ color: '#818995', marginLeft: 5, marginTop: 10 }}>
+								Short Description
+							</Text>
+						</View>
+					</View>
+
+					<View style={styles.slimTabCotainer}>
+						<LinearGradient
+							style={styles.slimTabCircle}
+							colors={['#32D1DC', '#6F9BF5']}>
+							<Image
+								source={Images.IcLicense}
+								style={{ width: 25, height: 25 }}
+								resizeMode="contain"
+							/>
+						</LinearGradient>
+						<View style={styles.vendorInfo}>
+							<Text style={{ fontSize: 18, marginLeft: 5, color: '#000' }}>
+								License
+							</Text>
+							<Text style={{ color: '#818995', marginLeft: 5, marginTop: 10 }}>
+								Short Description
+							</Text>
+						</View>
+					</View>
+
+					<View style={styles.slimTabCotainer}>
+						<LinearGradient
+							style={styles.slimTabCircle}
+							colors={['#FAD961', '#F76B1C']}>
+							<Image
+								source={Images.IcPrivacy}
+								style={{ width: 25, height: 25 }}
+								resizeMode="contain"
+							/>
+						</LinearGradient>
+						<View style={styles.vendorInfo}>
+							<Text style={{ fontSize: 18, marginLeft: 5, color: '#000' }}>
+								Privacy Policy
+							</Text>
+							<Text style={{ color: '#818995', marginLeft: 5, marginTop: 10 }}>
+								Short Description
+							</Text>
+						</View>
+					</View>
+        </View> */}
+				<CustomPage id={Config.appSettings.about_us_page_id} />
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	slimTabContainer: {
+		width: '90%',
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 15,
+	},
+
+	slimTabCotainer: {
+		width: '100%',
+		height: 95,
+		borderRadius: 3,
+		marginBottom: 10,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		backgroundColor: '#fff',
+		// shadowColor: '#000',
+		// shadowOpacity: 0.2,
+		// shadowOffset: {width: 0, height: 1}
+	},
+	slimTabCircle: {
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		marginLeft: 15,
+		marginRight: 15,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
