5c8b8b398f574aeac786bf728089060fee499673
ibnulK <ikayes114@gmail.com>
2019-05-23 15:34:20 +0600

    Remove FBLogo image and add no_internet image

--- src/common/Images.js
+++ src/common/Images.js
@@ -89,7 +89,7 @@ const Images = {
 	CategoryColorIcon: require('@images/category_color.png'),
 	VendorColorIcon: require('@images/vendor_color.png'),
 	ProductColorIcon: require('@images/product_color.png'),
-	FBLogo: require('@images/facebook-logo.png'),
+	NoConnection: require('@images/no_internet.png'),
 };
 
 export default Images;
