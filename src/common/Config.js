2496cefab72ab105e505b388fc192a572b521912
ibnulK <ikayes114@gmail.com>
2019-07-01 17:27:20 +0600

    Clean WooCommerce config

--- src/common/Config.js
+++ src/common/Config.js
@@ -9,21 +9,9 @@ export default {
 		 Step 1: change to your website URL and the wooCommerce API consumerKey
 		*/
 	WooCommerce: {
-		// url: 'http://ajaira.website/dokan-app',
-		// consumerKey: 'ck_4f6b5c9dd7e44a22a2ab9d0e9685b42429879e1a',
-		// consumerSecret: 'cs_8c8a7e181349b69ff32d21477ed94bcd832a8e26',
-
 		url: 'http://mobileapp.getdokan.com',
 		consumerKey: 'ck_e8cee3b732833e55c5a5e664a0a7de40eae05d28',
 		consumerSecret: 'cs_0b5b64944e8c319d6751ffe9c132f91deb3aca2c',
-
-		// url: 'http://dokan-mobile.test/',
-		// consumerKey: 'ck_058d37798c6d4fde333cce1e24864340d6a78017',
-		// consumerSecret: 'cs_6c3776e01ec312fcc2de6517edc76c139731469c',
-
-		// url: 'https://tedhut.com/',
-		// consumerKey: 'ck_288d122af9ac2a56977970fa404352a0ed32b6e6',
-		// consumerSecret: 'cs_ef48540035466d888768e10cc3571214b20e2af3',
 	},
 	/**
 		 Step 2: Setting Product Images
@@ -283,7 +271,6 @@ export default {
 				text: 'About Us',
 				routeName: 'AboutUs',
 				params: {
-					// url: 'http://ajaira.website/dokan-app',
 					isActive: false,
 				},
 				iconLeft: Icons.MaterialCommunityIcons.About,
@@ -417,9 +404,6 @@ export default {
 	// 	{
 	// 		label: Languages.About,
 	// 		routeName: "CustomPage",
-	// 		params: {
-	// 			url: "http://inspireui.com",
-	// 		},
 	// 	},
 	// ]
 };
