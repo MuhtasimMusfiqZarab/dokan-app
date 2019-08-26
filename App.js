3a42b1a119e2e75817578677578145a6b799c319
ibnulK <ikayes114@gmail.com>
2019-08-26 09:28:56 +0600

    Update OneSignal and Configure push notification for android

--- App.js
+++ App.js
@@ -16,16 +16,27 @@ import Router from './src/Router';
 export default class ReduxWrapper extends Component {
 	constructor(props) {
 		super(props);
-		OneSignal.init('265739e7-93c8-49f8-b1a0-f69990ce8458');
+		// OneSignal.init('265739e7-93c8-49f8-b1a0-f69990ce8458');
+		OneSignal.init('4c6151ae-422f-4c17-8884-40df44509e27');
+		OneSignal.inFocusDisplaying(2);
 	}
 
 	async componentDidMount() {
 		const notification = await getNotification();
 
+		// Check push notification and OneSignal subscription statuses
+		OneSignal.getPermissionSubscriptionState(status => {
+			console.log(status);
+		});
+
 		if (notification) {
-			OneSignal.removeEventListener('opened', this.onOpened);
+			console.log(notification);
+			OneSignal.setSubscription(true);
+			OneSignal.addEventListener('opened', this.onOpened);
 			OneSignal.addEventListener('received', this.onReceived);
 			OneSignal.addEventListener('ids', this.onIds);
+		} else {
+			OneSignal.setSubscription(false);
 		}
 
 		// set default Language for App
@@ -45,6 +56,9 @@ export default class ReduxWrapper extends Component {
 			OneSignal.removeEventListener('received', this.onReceived);
 			OneSignal.removeEventListener('ids', this.onIds);
 		}
+		// OneSignal.removeEventListener('opened', this.onOpened);
+		// OneSignal.removeEventListener('received', this.onReceived);
+		// OneSignal.removeEventListener('ids', this.onIds);
 	}
 
 	onReceived(notification) {
